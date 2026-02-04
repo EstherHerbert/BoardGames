ae026e13e900a86caf62206a37904dd938c09bc3
EstherHerbert <e.herbert@sheffield.ac.uk>
2026-02-04 21:10:56 +0000

    set user token

--- R/BoardGames-package.R
+++ R/BoardGames-package.R
@@ -6,3 +6,8 @@
 #' @importFrom magrittr %>%
 ## usethis namespace: end
 NULL
+
+# Options ----------------------------------------------------------------------
+.onLoad <- function(libname, pkgname) {
+  options(bggAnalytics.token = 'a323e76c-eb09-4549-b596-2e0976ce48bd')
+}
